1fb0a117b175d86fa087530540dca2b185aabe7b
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-03-30 13:20:27 -0400

    final commit

--- client/src/app/services/contact-list.service.ts
+++ client/src/app/services/contact-list.service.ts
@@ -12,8 +12,7 @@ export class ContactListService {
   private user: User;
   private authToken: any = null;
 
-  private endpoint =
-    "https://comp308-w2019-lesson10b.herokuapp.com/api/contact-list/";
+  private endpoint = "https://comp308-w2019-mean-portfolio.herokuapp.com/api/";
 
   //private endpoint = 'http://localhost:3000/api/contact-list/';
 
