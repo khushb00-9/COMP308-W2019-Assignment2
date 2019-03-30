87f9083f9ba3550f9d9a03ad9f95117db3349c35
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-03-30 12:33:11 -0400

    final commit

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   user: User;
   private authToken: any;
 
-  private endpoint = "https://comp308-w2019-assign2-mean.herokuapp.com/api/";
+  private endpoint = "https://comp308-w2019-mean-portfolio.herokuapp.com/api/";
   //private endpoint = "http://localhost:3000/api/";
 
   private httpOptions = {
