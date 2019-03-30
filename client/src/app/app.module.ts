79a493695a5bd2410864bb322475d7607461d7dc
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-03-29 22:26:49 -0400

    updates pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// Modules
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { BasePageComponent } from './partials/base-page/base-page.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+// Components
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./partials/header/header.component";
+import { FooterComponent } from "./partials/footer/footer.component";
+import { BasePageComponent } from "./partials/base-page/base-page.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { ServicesComponent } from "./pages/services/services.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+
+// Services
+import {
+  FlashMessagesModule,
+  FlashMessagesService
+} from "angular2-flash-messages";
+import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
 
 @NgModule({
   declarations: [
@@ -21,16 +32,20 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     BasePageComponent,
     HomeComponent,
     AboutComponent,
-    ServicesComponent,
     ProjectsComponent,
+    ServicesComponent,
     ContactComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [FlashMessagesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
