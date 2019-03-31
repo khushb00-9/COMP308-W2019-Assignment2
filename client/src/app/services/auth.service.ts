f601b23e1e19d6fa6c356abc34e1e2f47802df53
Khushboo Sakervala <ksakerva@my.centennialcollege.ca>
2019-03-30 22:53:12 -0400

    registration fixed

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   public user: User;
   private authToken: any;
 
-  private endpoint = "https://comp308-w2019-mean-portfolio.herokuapp.com//api/";
+  private endpoint = "https://comp308-w2019-mean-portfolio.herokuapp.com/api/";
 
   //private endpoint = 'http://localhost:3000/api/';
 
